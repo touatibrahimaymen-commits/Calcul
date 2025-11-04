00590b923f8dab602aa1663c640592ff9b82f62c
Aymen touati brahim <touatibrahimaymen@gmail.com>
2025-11-04 18:29:02 +0100

    god test

--- tests/test_calcul.py
+++ tests/test_calcul.py
@@ -2,4 +2,10 @@ from src.add_a_b_c import add
 
 
 def test_add():
-    add()
+    assert add(1,0,1)==2
+
+def test_add02():
+    assert add(5,3,1)==9
+
+def test_add03():
+    assert add(7,2,-1)==8
